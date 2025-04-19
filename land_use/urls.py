13991261c240d970189e6b3cbc42a2370ffe7cbc
Bantu-art <brianbaraza40@gmail.com>
2025-04-19 22:21:09 +0300

    Add URL pattern for image upload view and configure media serving in DEBUG mode

--- land_use/urls.py
+++ land_use/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from landapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.upload_images, name='upload'),
+    # We'll add 'compare/' path after we build the OpenCV logic
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
