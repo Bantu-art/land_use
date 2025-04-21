84a5601dc058c9df256477fa1bcd93698e355eac
Bantu-art <brianbaraza40@gmail.com>
2025-04-21 13:24:06 +0300

    Add image comparison functionality with processing logic

--- landapp/views.py
+++ landapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import ImageUploadForm
 from .models import AerialImage
+from .image_utils import process_images
+import os
+from django.conf import settings
 
 def upload_images(request):
     if request.method == 'POST':
@@ -14,3 +17,22 @@ def upload_images(request):
         form1 = ImageUploadForm(prefix="img1")
         form2 = ImageUploadForm(prefix="img2")
     return render(request, 'upload.html', {'form1': form1, 'form2': form2})
+
+def compare_images(request, img1_id, img2_id):
+    img1 = AerialImage.objects.get(id=img1_id)
+    img2 = AerialImage.objects.get(id=img2_id)
+    
+    # Get absolute paths of the images
+    img1_path = os.path.join(settings.MEDIA_ROOT, str(img1.image))
+    img2_path = os.path.join(settings.MEDIA_ROOT, str(img2.image))
+    
+    # Process the images
+    result_image = process_images(img1_path, img2_path)
+    
+    context = {
+        'img1': img1,
+        'img2': img2,
+        'result_image': result_image
+    }
+    
+    return render(request, 'compare.html', context)
